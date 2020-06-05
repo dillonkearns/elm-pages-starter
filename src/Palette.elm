41bbaf1897b6515221bbb2e7ab38fca04e9bfd3c
Phill Sparks <me@phills.me.uk>
2020-06-05 17:29:47 +0100

    Remove unused code from Palette
    - Added missing type annotation for `color`
    - Removed unused function `heading`

--- src/Palette.elm
+++ src/Palette.elm
@@ -1,35 +1,18 @@
-module Palette exposing (blogHeading, color, heading)
+module Palette exposing (blogHeading, color)
 
 import Element exposing (Element)
 import Element.Font as Font
 import Element.Region
 
 
+color :
+    { primary : Element.Color
+    , secondary : Element.Color
+    }
 color =
     { primary = Element.rgb255 5 117 230
     , secondary = Element.rgb255 0 242 96
     }
-
-
-heading : Int -> List (Element msg) -> Element msg
-heading level content =
-    Element.paragraph
-        ([ Font.bold
-         , Font.family [ Font.typeface "Raleway" ]
-         , Element.Region.heading level
-         ]
-            ++ (case level of
-                    1 ->
-                        [ Font.size 36 ]
-
-                    2 ->
-                        [ Font.size 24 ]
-
-                    _ ->
-                        [ Font.size 20 ]
-               )
-        )
-        content
 
 
 blogHeading : String -> Element msg
