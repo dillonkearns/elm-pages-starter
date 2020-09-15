81de3fd3e46b8ac9a6e0d47e20c8ba30826c8833
Dillon Kearns <dillon@dillonkearns.com>
2020-09-14 19:27:09 -0700

    Remove highlight.js from bundle.

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-import hljs from "highlight.js/lib/highlight";
-import "highlight.js/styles/github.css";
-import elm from 'highlight.js/lib/languages/elm';
-// we're just importing the syntaxes we want from hljs
-// in order to reduce our JS bundle size
-// see https://bjacobel.com/2016/12/04/highlight-bundle-size/
-hljs.registerLanguage('elm', elm);
-
-
 import "./style.css";
 // @ts-ignore
-window.hljs = hljs;
 const { Elm } = require("./src/Main.elm");
 const pagesInit = require("elm-pages");
 
