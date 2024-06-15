4dedf06cf589da2112219c8154952d86c236a102
Shahn Hogan <hogansa@outlook.com>
2024-06-15 21:46:49 +0100

    Add support for json and urlencoded body

--- adapters/express/middleware.mjs
+++ adapters/express/middleware.mjs
@@ -20,13 +20,24 @@ export default async (req, res, next) => {
 };
 
 const reqToElmPagesJson = (req) => {
-  const url = `${req.protocol}://${req.host}${req.originalUrl}`;
+  const { body, headers, method } = req;
+  const rawUrl = `${req.protocol}://${req.headers.host}${req.originalUrl}`;
+  
   return {
     requestTime: Math.round(new Date().getTime()),
-    method: req.method,
-    headers: req.headers,
-    rawUrl: url,
-    body: req.body,
+    method,
+    headers,
+    rawUrl,
+    body: body && isFormData(headers) ? toFormData(body) : body && JSON.stringify(body) || null,
     multiPartFormData: null,
   };
 };
+
+const isFormData = (headers) => headers['content-type'] === 'application/x-www-form-urlencoded';
+
+const toFormData = (body) => typeof body === 'string'
+    ? body
+    : Object.entries(body).reduce((formData, [key, value]) => {
+        formData.append(key, value);
+        return formData;
+    }, new URLSearchParams()).toString() || null;
--- adapters/express/server.mjs
+++ adapters/express/server.mjs
@@ -5,6 +5,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("dist"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(elmPagesMiddleware);
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
